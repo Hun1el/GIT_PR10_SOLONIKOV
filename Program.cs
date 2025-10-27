c5a5f763af69d4c54fc1db05de30db95e8f120fc
Solonikov <antonsolonikov@gmail.com>
2025-10-27 10:05:42 +0300

    Добавлен вывод ФИО и номера группы

--- Program.cs
+++ Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine(DateTime.Now);
-Console.WriteLine("ИС-22А, Солоников Антон Сергеевич")
+Console.WriteLine("ИС-22А, Солоников Антон Сергеевич");
